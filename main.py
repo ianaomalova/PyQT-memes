61ed4fdf1ed9d4c9c28ac7b0993a708c11119f9c
ianaomalova <69366481+ianaomalova@users.noreply.github.com>
2025-09-19 15:10:31 +0300

    feat: QMainWindow is replaced by QWidget

--- main.py
+++ main.py
@@ -1,28 +1,25 @@
 import sys
 
 from PyQt6.QtGui import QFont
-from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QHBoxLayout, QStackedWidget
+from PyQt6.QtWidgets import QApplication, QWidget, QHBoxLayout
 
 from navbar import NavBar
 from content_page import PageOne
 
-class MainWindow(QMainWindow):
+class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Семинар")
 
-        container = QWidget()
-        main_layout = QHBoxLayout(container)
+        main_layout = QHBoxLayout(self)
         main_layout.setSpacing(0)
         main_layout.setContentsMargins(0, 0, 0, 0)
-        self.setCentralWidget(container)
 
         self.navbar = NavBar()
         self.page1 = PageOne()
 
         main_layout.addWidget(self.navbar)
         main_layout.addWidget(self.page1)
-
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
